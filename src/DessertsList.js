dc4f308358a913b736e27af2f2da0779e4a72037
Dana Klymenko <bohdanamandziuk@gmail.com>
2023-06-07 14:50:55 +0300

    Updated key attribute for better performance

--- src/DessertsList.js
+++ src/DessertsList.js
@@ -1,10 +1,11 @@
 function DessertsList(props) {
-    let dessertItem = props.data.map(
-      (desserts, index) => {
-        const dessertText = `${desserts.name} - ${desserts.calories} cal`
-        return <li key={index}>{ dessertText}</li>
-      }
-    )
+  let dessertItem = props.data.filter(dessert => dessert.calories < 500).sort((a,b) => a.calories - b.calories).map(
+    (desserts) => {
+      const dessertText = `${desserts.name} - ${desserts.calories} cal`
+      //index item for a key in sorted list may impact the performance of the app, so if the position of the elements may be somehow altered (removed, added, sorted out by the user) it's better to avoid prescribing index itam to the key attribute
+      return <li key={desserts.name}>{ dessertText}</li>
+    }
+  )
     return <ul>
       {dessertItem}
     </ul>;
