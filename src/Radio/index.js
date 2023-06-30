8617b69641b34f17bd83d856a134c685fef45166
Dana Klymenko <bohdanamandziuk@gmail.com>
2023-06-30 13:10:18 +0300

    specified the logic for cloneElements and passed the correct props to input el

--- src/Radio/index.js
+++ src/Radio/index.js
@@ -3,10 +3,13 @@ import "./styles.css";
 
 export const RadioGroup = ({ onChange, selected, children }) => {
   // Use React.Children.map and React.cloneElement to clone the children
-  const RadioOptions = React.Children.map(children, (child, index) => {
-    return child;
-  });
   // and pass the correct props to each RadioOption
+  const RadioOptions = React.Children.map(children, (child) => {
+    return React.cloneElement (child, {
+        onChange,
+        checked: child.props.value === selected,
+    });
+  });
   //const RadioOptions = null;
 
   return <div className="RadioGroup">{RadioOptions}</div>;
@@ -14,11 +17,12 @@ export const RadioGroup = ({ onChange, selected, children }) => {
 
 export const RadioOption = ({ value, checked, onChange, children }) => {
   // Hook up the onChange handler to call the onChange prop passed to RadioGroup
-
   // Also, make sure to pass the correct checked prop to the input element
   return (
     <div className="RadioOption">
-      <input id={value} type="radio" name={value} />
+      <input id={value} type="radio" name={value} value={value} checked={checked} onChange={(e) => {
+        onChange(e.target.value)
+        }}/>
       <label htmlFor={value}>{children}</label>
     </div>
   );
